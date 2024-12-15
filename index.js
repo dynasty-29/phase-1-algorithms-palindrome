3907bd936fcd812a0879ecb699ae49b1f443cbd3
SonnieCodes <mgathoni.gathoni9@gmail.com>
2024-12-15 20:53:53 +0300

    palindrome

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const finalWord = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+  const reverseWord = finalWord.split('').reverse().join('');
+
+  return reverseWord === finalWord;
 }
 
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("a"));
+console.log(isPalindrome("robot"));
+console.log(isPalindrome("ab"));
 /* 
   Add your pseudocode here
+  the function should take a string which in this case its a word
+  to avoid case issues we convert it to lowercase and ensure we don't have special charactes
+  now we reverse the word 
+  if the original word and reverssed word read the same (hence use of strict equality) we should get true
+  otherwise false
+
 */
 
-/*
-  Add written explanation of your solution here
-*/
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
